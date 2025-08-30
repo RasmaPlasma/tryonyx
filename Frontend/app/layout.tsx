f5c07bf839aca61dfad6d53e4b99d21879a848e1
wolf <wolfgang.riegler@solvace-inc.com>
2025-08-30 17:32:45 +0200

    change name

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Mirrora',
+  description: 'Try on clothes and sustainably exchange fashion',
   generator: 'v0.app',
 }
 
